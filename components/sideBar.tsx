4b05d768bff10a94f6777a3d8e0bc7369618e4cc
johnelipse <johnelipse472@gmail.comgit config --global user.name johnelipseOmit --globalgit commit -meventgit config --global user.email johnelipse472@gmail.com>
2024-10-24 11:16:58 +0300

    new

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -25,7 +25,7 @@ export default function EnhancedSidebar() {
     { href: "#portfolio", icon: Briefcase, label: "PORTFOLIO" },
     { href: "#services", icon: BarChart2, label: "SERVICES" },
     { href: "#contact-me", icon: MapPin, label: "CONTACT ME" },
-    { href: "/dashboard", icon: SquareDashedKanban, label: "DASHBOARD" },
+    { href: "/login", icon: SquareDashedKanban, label: "DASHBOARD" },
   ];
 
   const socialLinks = [
